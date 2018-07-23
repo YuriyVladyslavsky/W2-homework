6b9e7279d0a8509ae0f26153b532c4cbea62a2b7
Yuriy <vladyslavsky@gmail.com>
2018-07-23 15:58:30 +0300

    Update task1-2 2functions Math..js

--- task1-2 2functions Math..js
+++ task1-2 2functions Math..js
@@ -1,10 +1,6 @@
-function getMaxOfArray(numbers) {
-  return Math.max.apply(Math, numbers);
-}
+const minMaxOfArray = arrayNumbers => ({
+max: Math.max(...arrayNumbers),
+min: Math.min(...arrayNumbers),
+});
 
-function getMinOfArray(numbers) {
-  return Math.min.apply(Math, numbers);
-}
-
-let arr = [24, 3, 57, 84, 77];
-console.log('Max number is: ' + getMaxOfArray(arr) + '\nMin number is: ' + getMinOfArray(arr));
+console.log(minMaxOfArray([32, 45, 97, 13, 29]));
